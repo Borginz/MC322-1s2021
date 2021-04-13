cb23abe1fecdbe2ea026d8a7370c4adf6c58aa61
Borginz <80779949+Borginz@users.noreply.github.com>
2021-04-13 16:14:54 -0300

    Add files via upload

---
+++ lab03/src/mc322/lab03/Animacao.java
@@ -0,0 +1,35 @@
+package Lab03;
+
+public class Animacao {
+	int tam_aquario, lombriga, pos_ini,tam_string, acao_atual=0;
+	String animacao, aux, animacao_rv, aquario;
+	AquarioLombriga lomb_atual;
+	Animacao(String animacao){
+		animacao_rv = animacao;
+		aux = animacao.substring(0,2);
+		tam_aquario = Integer.parseInt(aux);
+		aux = animacao.substring(2,4);
+		lombriga = Integer.parseInt(aux);
+		aux = animacao.substring(4,6);
+		pos_ini = Integer.parseInt(aux);
+		this.animacao = animacao.substring(6);
+		System.out.println("passa como parametro " + animacao_rv);
+		lomb_atual = new AquarioLombriga(tam_aquario, lombriga, pos_ini);
+	}
+	
+	String apresenta(){
+		return lomb_atual.aquario;
+	}
+	
+	void passo() {
+			switch(animacao.charAt(acao_atual)) {
+			case 'C' : lomb_atual.crescer();break;
+			case 'M' : lomb_atual.mover();break;
+			case 'V' : lomb_atual.virar();break;
+			}
+			acao_atual++;
+			System.out.println("passo - executa primeira ação " + animacao.charAt(acao_atual-1));
+		}
+	}
+
+
--- lab03/src/mc322/lab03/AppLab03.java
+++ lab03/src/mc322/lab03/AppLab03.java
@@ -3,7 +3,7 @@ package Lab03;
 public class AppLab03 {
 	public static void main(String args[]) {
 		String entrada_str = "080403MCMVM";
-		Alegrando alegre = new Alegrando(entrada_str);
+		Animacao alegre = new Animacao(entrada_str);
 		alegre.apresenta();
 		System.out.println(alegre.apresenta());
 		alegre.passo();
